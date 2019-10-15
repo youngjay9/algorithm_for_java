9cfcbe7a88ad36a9d67ff0d061abaad6fa37eedb
jay <youngjay9@gmail.com>
2019-10-16 06:50:33 +0800

    add findPrimeNum

---
+++ src/main/java/prime/App.java
@@ -0,0 +1,49 @@
+package prime;
+
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+  private static Logger logger = LoggerFactory.getLogger(App.class);
+
+  private static int[] primes = {2, 3, 5, 7};
+
+  private static boolean chkNumIsPrime(int num) {
+    boolean result = false;
+
+    for(int p: primes){
+      if(num == p){
+        result = true;
+        break;
+      }
+      else{
+        int mod = num % p;
+        if(mod > 0){
+          result = true;
+        }
+        else if(mod == 0){
+          result = false;
+          break;
+        }
+      }
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+
+    for (int i = 0; i < 31; i++) {
+        if(i == 0 || i ==1){
+          continue;
+        }
+
+        if(chkNumIsPrime(i)){
+          logger.info("prime:"+i);
+        }
+    }
+  }
+
+}
