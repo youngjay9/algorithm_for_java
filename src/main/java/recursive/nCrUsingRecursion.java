529a6b25efc54cd77dd1fcfeaafbfd2bc57c1ed5
Jay Hung <youngjay9@gmail.com>
2020-10-11 15:23:55 +0800

    利用 recursion 取得組合數

--- src/main/java/recursive/nCrUsingRecursion.java
+++ src/main/java/recursive/nCrUsingRecursion.java
@@ -8,20 +8,22 @@ public class nCrUsingRecursion {
   private static Logger logger = LoggerFactory.getLogger(nCrUsingRecursion.class);
 
   /**
-   * 利用帕斯卡三角形取得 recursive 的關係, 看 udemy 58 筆記.
-    * @param n
+   * 利用帕斯卡三角形取得 recursive 的關係, 看 udemyMastering Data Structures & Algorithms using C and C++ 您的進度 58
+   * 筆記.
+   *
+   * @param n
    * @param r
    * @return
    */
-  static  int c(int n, int r){
-    if(r == 0 || n == r){
-        return 1;
+  static int c(int n, int r) {
+    if (r == 0 || n == r) {
+      return 1;
     }
 
-    return c(n-1, r-1) + c(n-1, r);
+    return c(n - 1, r - 1) + c(n - 1, r);
   }
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     logger.info("result: " + c(4, 3));
   }
 
